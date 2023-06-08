c2db893f02f4b26ed6d32fb0c1784080b344a923
AlexGeorge42580043 <georgealex42580043@gmail.com>
2023-06-08 13:19:43 -0300

    Proyecto listo para ejecutar

--- Ejercicio b4/Ejercicio b4/Form1.vb
+++ Ejercicio b4/Ejercicio b4/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
     Private Sub btEjecutar_Click(sender As Object, e As EventArgs) Handles btEjecutar.Click
-
+        For n = 0 To 2000 Step 2
+            lstMostrarPares.Items.Add("" & n)
+        Next
     End Sub
 End Class
